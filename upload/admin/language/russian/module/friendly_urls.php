35158e62fb89f904ad39dd4148d966d90787fe18
Stas Pazhoha <staconik@gmail.com>
2016-04-16 10:49:38 +0300

    названия

--- upload/admin/language/russian/module/friendly_urls.php
+++ upload/admin/language/russian/module/friendly_urls.php
@@ -5,5 +5,5 @@ $_['heading_title'] = "Friendly URLs";
 
 //Text
 $_['text_options'] = "Опции";
-$_['text_url_added'] = "<span class=\"text-success\"><i class=\"fa fa-check\"></i> Продукт <a href=\"%s\">%s</a> успешно изменен.</span><br/>";
-$_['text_url_exist'] = "<span class=\"text-info\"><i class=\"fa fa-exclamation\"></i> seo url для продукта <a href=\"%s\">%s</a> уже сгенерирован.</span><br/>";
+$_['text_url_added'] = "<span class=\"text-success\"><i class=\"fa fa-check\"></i> Элемент <a href=\"%s\">%s</a> успешно изменен.</span><br/>";
+$_['text_url_exist'] = "<span class=\"text-info\"><i class=\"fa fa-exclamation\"></i> seo url для элемента <a href=\"%s\">%s</a> уже сгенерирован.</span><br/>";
